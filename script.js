3ac828124ccff7b87281656c43a9e9fde0fabfbd
aayus-2003 <139258276+aayus-2003@users.noreply.github.com>
2024-01-31 21:45:27 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+let a = document.getElementById("dark")
+let b = document.body
+
+a.addEventListener('click',function(){
+    b.classList.toggle("black")
+})
+
+
+
+let c = document.getElementById("ayu")
+
+
+let d = document.getElementsByClassName("sort")[0]
+
+
+c.addEventListener('click',function(){
+   d.classList.toggle("yup")
+})
